776d72904a41172261bb18c49a9fafdd3beef07e
Hugo Rocha <hugorochalves@gmail.com>
2022-10-13 23:25:31 -0300

    Adicionar arquivos de projeto.

---
+++ Produto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace This
+{
+    public class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        public Produto()
+        {
+            Quantidade = 0;
+        }
+        public Produto(string nome, double preco) : this()
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco)
+        {
+            Quantidade = quantidade;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+namespace Course
+{
+    class Produto
+    {
+        /* 
+        • É uma referência para o próprio objeto
+        • Usos comuns:
+        • Diferenciar atributos de variáveis locais (Java)
+        • Referenciar outro construtor em um construtor
+        • Passar o próprio objeto como argumento na chamada de um método ou construtor
+        */
+    }
+}
